ff99742981e06933374294c2035f1bf9215277c3
Suzuna Arakaki <suzuna.arakaki@gmail.com>
2025-09-26 11:51:57 -0700

    exercise 04 is done

--- ex04.js
+++ ex04.js
@@ -4,8 +4,16 @@
 // Test your function by calculating the sum of the array and logging the result to the console.
 
 function sumOfDigits(n) {
-  if (n.length  0){
-
+  //if loop 
+  //define the base case n.length === 0 rturn 0
+  if (n.length === 0) {
+    return 0;
+  } else {
+    //here we use recursive case
+    //creat the sentence like -> first element of the array + rest of the array
+    //first element n[0] + rest of the arrya n.slice(1) <-slice method returns new array whitout the first element
+    //we call the function sumOfDigits with the rest of the array
+    return n[0] + sumOfDigits(n.slice(1));
   }
 }
 
