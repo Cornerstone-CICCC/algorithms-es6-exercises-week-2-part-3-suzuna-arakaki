c060d0987fb4202c9074778a1ef17fa38ecfe17c
Suzuna Arakaki <suzuna.arakaki@gmail.com>
2025-09-25 23:04:55 -0700

    exercise 03 is done

--- ex03.js
+++ ex03.js
@@ -1,9 +1,16 @@
 // Exercise 3: Set
-// Instructions: Create a Set named `uniqueNumbers` and add some numbers to it using .add(), including some duplicates. 
+// Instructions: Create a Set named `uniqueNumbers` and add some numbers to it using .add(), 
+// including some duplicates. 
 // Verify that the Set only keeps unique values by logging it to the console.
 
 const uniqueNumbers = new Set();
 
-// Your code here
+uniqueNumbers.add(5);
+
+uniqueNumbers.add(10);
+
+uniqueNumbers.add(5); // Duplicate won't be added
+
+uniqueNumbers.add(3);
 
 console.log(uniqueNumbers);
